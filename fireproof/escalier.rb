4bdea87a6d348762f6b09177cf190791ef80c975
alatheaferrand <83938198+alatheaferrand@users.noreply.github.com>
2021-05-15 05:10:23 -1000

    Second commit 2.0

--- fireproof/escalier.rb
+++ fireproof/escalier.rb
@@ -1,12 +1,20 @@
+#gestion d'erreur
 if (ARGV.length != 1)
   puts "#{__FILE__} requires one argument: #{__FILE__} number_of_steps"
   exit
 end
 
+#récup entrée 1 entier pour boucle
 marches_nb=ARGV[0].to_i
 
+=begin
+ex: escalier.rb 4   doit afficher un escalier à 4 niveaux avec des dièses selon le nombre en entrée
+   #
+  ##
+ ###
+####
+=end
 i=0
-
 while i < marches_nb
   i+=1
   puts " "*(marches_nb - i) + "\#"*(i)
