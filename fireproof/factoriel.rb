8d0c00b79cdeb71830ca124bcf5196066d7cce74
Alathea Ferrand <alatheaferrand@gmail.com>
2021-05-16 04:58:38 -1000

    factoriel 1.0

---
+++ fireproof/factoriel.rb
@@ -0,0 +1,13 @@
+if (ARGV.length < 1)
+  puts "#{__FILE__} requires one argument: #{__FILE__} number"
+  exit
+end
+
+number=ARGV[0].to_i
+
+def factoriel(n)
+  return 1 if n <= 1 #met fin à la boucle
+  n*factoriel(n-1)
+end
+
+print factoriel(number)
